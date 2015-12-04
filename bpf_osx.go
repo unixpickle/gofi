621ace482553ac96e95ca4d358b121b53933390c
Alex Nichol <unixpickle@gmail.com>
2015-12-03 21:59:53 -0500

    osx: bpf setting a device works
    I tested this by running a program as root and not as root, giving it a real interface name and a made up interface name.

--- bpf_osx.go
+++ bpf_osx.go
@@ -17,7 +17,7 @@ const ioctlBIOCSETIF = 0x8020426c
 type bpfHandle int
 
 func newBpfHandle() (bpfHandle, error) {
-	res, err := unix.Open("/dev/bpf", 0, 0)
+	res, err := unix.Open("/dev/bpf", unix.O_RDWR, 0)
 	if err == nil {
 		return bpfHandle(res), nil
 	} else if err == unix.EACCES {
@@ -26,7 +26,7 @@ func newBpfHandle() (bpfHandle, error) {
 	i := 0
 	for {
 		devName := "/dev/bpf" + strconv.Itoa(i)
-		res, err := unix.Open(devName, 0, 0)
+		res, err := unix.Open(devName, unix.O_RDWR, 0)
 		if err == nil {
 			return bpfHandle(res), nil
 		} else if err == unix.EACCES {
@@ -42,17 +42,23 @@ func newBpfHandle() (bpfHandle, error) {
 func (b bpfHandle) SetInterface(name string) error {
 	data := make([]byte, 100)
 	copy(data, []byte(name))
-	_, err := b.ioctlWithData(ioctlBIOCSETIF, data)
-	if err == unix.ENXIO {
+	if ok, err := b.ioctlWithData(ioctlBIOCSETIF, data); ok {
+		return nil
+	} else if err == unix.ENXIO {
 		return errors.New("no such device: " + name)
 	} else if err == unix.ENETDOWN {
 		return errors.New("interface is down: " + name)
+	} else {
+		return err
 	}
-	return err
 }
 
-func (b bpfHandle) ioctlWithData(command int, data []byte) (int, syscall.Errno) {
-	r, _, err := unix.Syscall(unix.SYS_IOCTL, uintptr(b), uintptr(command),
+func (b bpfHandle) ioctlWithData(command int, data []byte) (ok bool, err syscall.Errno) {
+	_, _, err = unix.Syscall(unix.SYS_IOCTL, uintptr(b), uintptr(command),
 		uintptr(unsafe.Pointer(&data[0])))
-	return int(r), err
+	if err != 0 {
+		return
+	} else {
+		return true, 0
+	}
 }
